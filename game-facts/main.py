ae9b0369d1f3d52f2291de8310777adb66d3f1f2
Kumar Aggarwal <kumar.aggarwal@telekom-digital.com>
2021-10-04 17:37:02 +0530

    added sonic facts

--- game-facts/main.py
+++ game-facts/main.py
@@ -213,7 +213,33 @@ async def read_item(limit: int = -1, random: bool = False):
     requiredFacts = []
     for i in range(limit):
         requiredFacts.append(allFacts[i])
-    return requiredFacts     
+    return requiredFacts  
+
+
+
+#sonic
+@app.get("/sonic/")
+async def read_item(limit: int = -1, random: bool = False):
+    with open('sonic.json', "r", encoding='utf-8') as json_file:
+        allFacts = json.load(json_file, strict=False)
+    countAllFacts = len(allFacts)
+    if limit < 0 or limit >= countAllFacts:
+        if random == False:
+            return allFacts
+        Rand.shuffle(allFacts)
+        return allFacts
+    if limit == 0:
+        return []
+    if random:
+        Rand.shuffle(allFacts)
+        requiredFacts = []
+        for i in range(limit):
+            requiredFacts.append(allFacts[i])
+        return requiredFacts
+    requiredFacts = []
+    for i in range(limit):
+        requiredFacts.append(allFacts[i])
+    return requiredFacts         
 
 
 
